45b85c05c9aa2e5bbb1bbb8b96cc08c2d50e6801
thrx <thrx>
2020-03-20 21:07:16 -0400

    demand form
    fix some css
    
    mask svg
    
    call
    
    cleaning up some files
    
    cleaning up routes
    
    some rhetoric
    
    remove donations

--- controllers/home.js
+++ controllers/home.js
@@ -2,26 +2,15 @@
  * GET /
  * Home page.
  */
+
 exports.index = (req, res) => {
-  res.render('call-1', {
-    title: 'Donate Now!'
-  });
-};
-
-exports.demo = (req, res) => {
-  res.render('demo', {
+  res.render('index', {
     title: 'Lading Page'
   });
 };
 
-exports.call1 = (req, res) => {
-  res.render('call-1', {
-    title: 'Lading Page'
-  });
-};
-
-exports.providers = (req, res) => {
-  res.render('providers', {
-    title: 'Provider Donations'
+exports.learn_more = (req, res) => {
+  res.render('learn-more', {
+    title: 'Learn More'
   });
 };
